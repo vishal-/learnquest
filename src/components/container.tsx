108e6d1db220361fc17285ed9cd725ac05e7a16a
Vishal Gupta <ims.vishal@gmail.com>
2025-07-30 09:41:20 +0530

    Added favicon

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,16 +1,6 @@
 // Container component that serves as a wrapper with default styling
-const Container: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return (
-    <div
-      style={{
-        maxWidth: "1200px",
-        margin: "0 auto",
-        padding: "0 20px"
-      }}
-    >
-      {children}
-    </div>
-  );
+const Container: React.FC = () => {
+  return <div>This is the Container component.</div>;
 };
 
 export default Container;
