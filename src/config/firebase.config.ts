75405add74a2dc0eeba33c21ac3fef388367184f
Vishal Gupta <ims.vishal@gmail.com>
2025-08-25 22:28:28 +0530

    Updated project to read from firestore

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDe9Z-pFZ0hSzN_kWZWPVibTPKRAvWNqpk",
+  authDomain: "learn-quest-482dc.firebaseapp.com",
+  projectId: "learn-quest-482dc",
+  storageBucket: "learn-quest-482dc.firebasestorage.app",
+  messagingSenderId: "351803189827",
+  appId: "1:351803189827:web:ba7dac6a04f23a749902f6",
+  measurementId: "G-MLFRGPRR8J"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
